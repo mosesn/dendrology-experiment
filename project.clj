8545ce9458638eb7d7c04b7c2506bbfb120a4620
Moses Nakamura <mnakamura@twitter.com>
2014-06-12 23:45:26 -0700

    changed url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dendrology-examples "0.0.1"
   :description "Trying examples from Brandon Bloom's talk on dendrology"
   :main com.mosesn.dendrology.driver
-  :url "https://github.com/mosesn/dendrology"
+  :url "https://github.com/mosesn/dendrology-experiment"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.match "0.2.1"]])
